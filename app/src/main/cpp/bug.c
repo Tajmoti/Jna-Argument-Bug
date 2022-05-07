9bbb951f24108b5771a320654fa21f95acca9e8d
Timotej Šulík <timotej.sulik@gmail.com>
2022-05-07 14:43:13 +0200

    Add additional debug output

--- app/src/main/cpp/bug.c
+++ app/src/main/cpp/bug.c
@@ -1,6 +1,11 @@
+#include <android/log.h>
 #include "bug.h"
 
+#define TAG "JNA-BUG-NATIVE"
+
 bool hello(UINT16 argument) {
+    __android_log_print(ANDROID_LOG_WARN, TAG, "argument value as %%#010x=%#010x %%d=%d, as %%u=%u, as %%hu=%hu", argument, argument, argument, argument);
+    __android_log_print(ANDROID_LOG_WARN, TAG, "CONSTANT value as %%#010x=%#010x %%d=%d, as %%u=%u, as %%hu=%hu", CONSTANT, CONSTANT, CONSTANT, CONSTANT);
     switch (argument) { // NOLINT(hicpp-multiway-paths-covered)
         case CONSTANT:
             return true;
